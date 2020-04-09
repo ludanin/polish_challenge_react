a7929bf42950930e4e12ad755a5e2c3fc2a65c51
Lukas Danin <lukas.danin@gmail.com>
2020-04-09 00:55:20 -0300

    Adds Redux' reducers, store and sagas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import reducers from "reducers/";
+import sagas from "sagas/";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(sagas);
 
 const App: React.FC = () => {
   return (
-    <div className="APP">
-      {/* TODO */}
-    </div>
+    <Provider store={store}>
+      <div className="APP">
+        {/* TODO  */}
+      </div>
+    </Provider>
   );
 };
 
