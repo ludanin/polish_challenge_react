3ac9929e235b5339195d50b5d51d5ec20287f21d
Lukas Danin <lukas.danin@gmail.com>
2020-04-09 02:54:06 -0300

    Sketches the Footer, saves variables

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="FOOTER">
+      {/* TODO */}
+      <div className={styles.something}/>
+    </div>
+  );
+};
+
+export default Footer;
