2a1d4e29f96f7b5ca27aebb70ebf8d409e75246d
Lukas Danin <lukas.danin@gmail.com>
2020-04-09 00:54:43 -0300

    Adds a saga that keeps fetching data from server

---
+++ src/sagas/index.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosResponse } from "axios";
+import { all, delay, put, takeLatest } from "redux-saga/effects";
+
+import types from "actions/types";
+
+/**
+ * Keeps FETCH_DATA running continously until the application is closed
+ */
+function* watchFETCH_DATA() {
+  yield takeLatest(types.FETCH_DATA, FETCH_DATA);
+} function* FETCH_DATA() {
+  try {
+    yield delay(1000);
+
+    const response: AxiosResponse<string> = yield axios.get(
+      "http://localhost:8080/api/get",
+    );
+
+    console.log(response);
+
+    if (response.status === 200) {
+      yield put({
+        type: types.FETCH_DATA,
+        data: response.data,
+      });
+    } else {
+      yield put({
+        type: types.FETCH_DATA,
+        data: [],
+      });
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+export default function* sagas() {
+  yield all([
+    watchFETCH_DATA(),
+  ]);
+}
